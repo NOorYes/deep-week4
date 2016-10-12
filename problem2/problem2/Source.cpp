1743d31f2a754444733470b38e21c3d7d5e1cc1b
dsaf2007 <dsaf2007@naver.com>
2016-10-12 13:54:59 +0900

    yeah

---
+++ problem2/problem2/Source.cpp
@@ -0,0 +1,22 @@
+template <typename T>
+class VectorT2D
+{
+private:
+	T x_, y_;
+};
+
+
+class VectorInt2D
+{
+	int x_, y_;
+};
+
+class VectorFloat2D
+{
+	float x_, y_;
+};
+
+class VectorDouble2D
+{
+	double x_, y_;
+};
